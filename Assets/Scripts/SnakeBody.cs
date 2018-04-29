2645991d4402b88f3c68448d4a3e2509068858f5
KirioO73 <quentin.codelupi@hotmail.fr>
2018-04-29 19:14:03 +0200

    Initial commit

---
+++ Assets/Scripts/SnakeBody.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SnakeBody : MonoBehaviour {
+
+	private int myOrder;                         //position dans la liste des corps
+	public Transform head;                       // tete a la quelle la boule du corps est attachée
+
+	void Start(){
+		head = GameObject.FindGameObjectWithTag("Player").gameObject.transform;                           //recuperation de la tete
+		for(int i = 0; i < head.GetComponent<SnakeMovements>().bodyParts.Count; i++){                     //parcour de la liste du corps
+			if(gameObject == head.GetComponent<SnakeMovements>().bodyParts[i].gameObject){                //ajoute la boule dans la liste, et met a jour le INT position de la boulle
+				myOrder = i;
+			}
+		}
+	}
+
+	private Vector3 movementVelocity;
+	[Range(0.0f,1.0f)]
+	public float overTime = 0.5f;                //Vitesse a la quelle le corps ratrappe la tete (la distance augmente avec la vitesse de la tete)
+
+	void FixedUpdate(){
+		if(myOrder == 0){     //si premier de la liste, je suis la tete
+			transform.position = Vector3.SmoothDamp(transform.position, head.position, ref movementVelocity, overTime);
+		}
+		else{ // sinon je suis la boule devant moi
+			transform.position = Vector3.SmoothDamp(transform.position, head.GetComponent<SnakeMovements>().bodyParts[myOrder-1].position, ref movementVelocity, overTime);
+		}
+	}
+}
---
+++ Assets/Scripts/SnakeMovements.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SnakeMovements : MonoBehaviour {
+
+	public List<Transform> bodyParts = new List<Transform>(); //Liste des boules du corps du snake
+
+	void Start () {
+		
+	}
+	
+	void Update () {
+		//Commandes du controle du snake Q et D dans ce cas
+		if (Input.GetKey (KeyCode.Q)) {
+			currentRotation += RotationSensitivity * Time.deltaTime;
+		}
+		if (Input.GetKey (KeyCode.D)) {
+			currentRotation -= RotationSensitivity * Time.deltaTime;
+		}
+	}
+
+
+	public float speed = 3.5f; //Vitesse de la tete du snake
+
+	public float currentRotation;                  // valeur de la rotation actuelle 
+	public float RotationSensitivity = 50.0f;	   //Sensibilité de la rotation
+
+	void FixedUpdate(){                            //check 3 fois par frame
+		MoveForward ();                            // Fait avancer le snake
+		Rotation ();                               // gere la rotation du snake
+	}
+
+
+	void MoveForward (){                          // Mouvement vers l'avant
+		transform.position += transform.up * speed * Time.deltaTime;
+	}
+
+	void Rotation(){                                          //applique une rotation autour de l'axe Z 
+		transform.rotation = Quaternion.Euler (new Vector3 (transform.rotation.x, transform.rotation.y, currentRotation));
+	}
+
+
+	public Transform bodyObject;                     // attachement du prefab "body" pour gerer la generation apres chaque consomation de pomme
+
+	void OnCollisionEnter2D(Collision2D other) {                        //2D TRES IMPORTANT 
+		if (other.transform.tag == "Apple") {                             // check le tag de l'objet en collision
+			Destroy(other.gameObject);                                    // detruit la pomme touchée
+			  
+			if (bodyParts.Count == 0) {                                   //si pas de corps
+				Vector2 currentPos = transform.position;                  // position de la premiere boule du coups sur la tete
+				Transform newBodyPart = Instantiate (bodyObject, currentPos, Quaternion.identity) as Transform;        //On instantie
+				bodyParts.Add (newBodyPart);                                                                           // ajout dans la liste des bouts de corps
+				newBodyPart.GetComponent<SpriteRenderer> ().sortingOrder = 0;										   //On met a la position 0 dans le layer 
+			}else {                                                             // si on a deja des boules du corps
+				Vector3 currentPos = bodyParts[bodyParts.Count-1].position;     // on fait apparaitre la prochaine boule aux coordonnées de la dernierre boule du corps
+				Transform newBodyPart = Instantiate (bodyObject, currentPos, Quaternion.identity) as Transform; //On instantie
+				bodyParts.Add (newBodyPart);                                                  					 // ajout dans la liste des bouts de corps
+				majLayers ();													//Decalage de toutes les autres boules du couprs dans le layer
+				newBodyPart.GetComponent<SpriteRenderer> ().sortingOrder = 0;	//On place la dernierre boule en dessoud de toutes les autres
+			}
+		}
+	}
+
+	void majLayers(){                    // decalage des boules du corps pour faire apparaitre une nouvelle en dessous (couche 0 du layer)
+		
+		for (int i = 0; i < bodyParts.Count; i++) {
+			bodyParts[i].GetComponent<SpriteRenderer> ().sortingOrder = bodyParts[i].GetComponent<SpriteRenderer> ().sortingOrder+1;
+		}
+	}
+		
+}
